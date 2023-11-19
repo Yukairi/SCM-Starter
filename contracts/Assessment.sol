6ea46864bff0e6d7366a9ad2a845b752c20833ea
Yukairi <yunnobelleza@gmail.com>
2023-11-19 21:34:24 +0800

    Onnuy

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -57,4 +57,22 @@ contract Assessment {
         // emit the event
         emit Withdraw(_withdrawAmount);
     }
+
+    function assertExample(uint x) public pure returns (uint) {
+        // Use assert() to check for conditions that should never be false.
+        // cannot be equal to 10 or it will cause an error
+        assert(x != 10);
+
+        // Perform some operations.
+        return 1000 / x;
+    }
+
+    function revertExample(uint256 _value) external pure returns(bool) {
+        // if _value is not equal to 10, the function will return true.
+        // If _value is equal to 10, the transaction will be reverted.
+        if (_value == 10) {
+            revert("Value must not be 10");
+        }
+        return true;
+    }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import {useState, useEffect} from "react";
-import {ethers} from "ethers";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
 import atm_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
 
 export default function HomePage() {
@@ -7,41 +7,41 @@ export default function HomePage() {
   const [account, setAccount] = useState(undefined);
   const [atm, setATM] = useState(undefined);
   const [balance, setBalance] = useState(undefined);
+  const [inputValue, setInputValue] = useState(""); // State to manage user input
 
   const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
   const atmABI = atm_abi.abi;
 
-  const getWallet = async() => {
+  const getWallet = async () => {
     if (window.ethereum) {
       setEthWallet(window.ethereum);
     }
 
     if (ethWallet) {
-      const account = await ethWallet.request({method: "eth_accounts"});
+      const account = await ethWallet.request({ method: "eth_accounts" });
       handleAccount(account);
     }
-  }
+  };
 
   const handleAccount = (account) => {
     if (account) {
-      console.log ("Account connected: ", account);
+      console.log("Account connected: ", account);
       setAccount(account);
-    }
-    else {
+    } else {
       console.log("No account found");
     }
-  }
+  };
 
-  const connectAccount = async() => {
+  const connectAccount = async () => {
     if (!ethWallet) {
-      alert('MetaMask wallet is required to connect');
+      alert("MetaMask wallet is required to connect");
       return;
     }
-  
-    const accounts = await ethWallet.request({ method: 'eth_requestAccounts' });
+
+    const accounts = await ethWallet.request({ method: "eth_requestAccounts" });
     handleAccount(accounts);
-    
-    // once wallet is set we can get a reference to our deployed contract
+
+    // once the wallet is set we can get a reference to our deployed contract
     getATMContract();
   };
 
@@ -49,44 +49,50 @@ export default function HomePage() {
     const provider = new ethers.providers.Web3Provider(ethWallet);
     const signer = provider.getSigner();
     const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
- 
-    setATM(atmContract);
-  }
 
-  const getBalance = async() => {
+    setATM(atmContract);
+  };
+
+  const getBalance = async () => {
     if (atm) {
       setBalance((await atm.getBalance()).toNumber());
     }
-  }
+  };
 
-  const deposit = async() => {
+  const deposit = async () => {
     if (atm) {
       let tx = await atm.deposit(1);
-      await tx.wait()
+      await tx.wait();
       getBalance();
     }
-  }
+  };
 
-  const withdraw = async() => {
-    if (atm) {
-      let tx = await atm.withdraw(1);
-      await tx.wait()
-      getBalance();
+  const withdraw = async () => {
+    try {
+      if (atm) {
+        let tx = await atm.withdraw(1);
+        await tx.wait();
+        getBalance();
+      }
+    } catch (error) {
+      console.error("Withdraw error:", error.message);
     }
-  }
+  };
 
   const initUser = () => {
-    // Check to see if user has Metamask
+    // Check to see if the user has Metamask
     if (!ethWallet) {
-      return <p>Please install Metamask in order to use this ATM.</p>
+      return <p>Please install Metamask to use this ATM.</p>;
     }
 
-    // Check to see if user is connected. If not, connect to their account
+    // Check to see if the user is connected. If not, connect to their account
     if (!account) {
-      return <button onClick={connectAccount}>Please connect your Metamask wallet</button>
+      return (
+        <button onClick={connectAccount}>Please connect your Metamask wallet</button>
+      );
     }
 
-    if (balance == undefined) {
+    if (balance === undefined) {
       getBalance();
     }
 
@@ -96,22 +102,77 @@ export default function HomePage() {
         <p>Your Balance: {balance}</p>
         <button onClick={deposit}>Deposit 1 ETH</button>
         <button onClick={withdraw}>Withdraw 1 ETH</button>
+        <form>
+        <label>
+          Enter a number:
+          <input
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button type="button" onClick={testRevertExample}>
+          Test Revert Example
+        </button>
+        <button type="button" onClick={testAssertExample}>
+          Test Assert Example
+        </button>
+      </form>
       </div>
-    )
-  }
+    );
+  };
 
-  useEffect(() => {getWallet();}, []);
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const testRevertExample = async () => {
+    try {
+      if (atm) {
+        // Check if inputValue is a valid number before proceeding
+        if (!isNaN(inputValue)) {
+          let result = await atm.revertExample(inputValue);
+          console.log("Revert Example Result:", result);
+        } else {
+          console.error("Invalid input for revertExample.");
+        }
+      }
+    } catch (error) {
+      console.error("Revert Example error:", error.message);
+    }
+  };
+
+  const testAssertExample = async () => {
+    try {
+      if (atm) {
+        // Check if inputValue is a valid number before proceeding
+        if (!isNaN(inputValue)) {
+          let result = await atm.assertExample(inputValue);
+          console.log("Assert Example Result:", result);
+        } else {
+          console.error("Invalid input for assertExample.");
+        }
+      }
+    } catch (error) {
+      console.error("Assert Example error:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    getWallet();
+  }, []);
 
   return (
     <main className="container">
-      <header><h1>Welcome to the Metacrafters ATM!</h1></header>
+      <header>
+        <h1>Welcome to the Metacrafters ATM!</h1>
+      </header>
       {initUser()}
       <style jsx>{`
         .container {
-          text-align: center
+          text-align: center;
         }
-      `}
-      </style>
+      `}</style>
     </main>
-  )
+  );
 }
